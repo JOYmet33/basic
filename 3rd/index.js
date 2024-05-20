5c5b07f0b74b67342b0715afff98598a334b4d1a
joymet33 <joymet33@gmail.com>
2024-05-20 18:07:53 +0900

    feat: 5번 문제 해결

--- 3rd/index.js
+++ 3rd/index.js
@@ -1,9 +1,11 @@
+// 문제 1)
 const users = [
   { name: "철수", age: 19, gender: "남" },
   { name: "짱구", age: 20, gender: "남" },
   { name: "유리", age: 21, gender: "여" },
 ];
 
+// 문제 2)
 const userList = document.querySelector(".userList");
 
 users.forEach((user) => {
@@ -22,3 +24,24 @@ console.log(findGirl);
 // 문제 4)
 const findBoys = users.filter((user) => user.gender === "남");
 console.log(findBoys);
+
+// 문제 5)
+const plusAges = users.map((user) => {
+  return {
+    name: user.name,
+    age: user.age + 10,
+    gender: user.gender,
+  };
+});
+console.log(plusAges);
+
+// 문제 5) 번외 -- map 연습하기
+// 모든 데이터의 이름에 "님" 붙이기
+const plusName = users.map((user) => {
+  return {
+    name: user.name + "님", //글자를 붙이려면 더하기 기호를 써야 붙는구나!
+    age: user.age,
+    gender: user.gender,
+  };
+});
+console.log(plusName);
